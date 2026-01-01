0313422fb82a14d894b0657a62f8aae6aa25fb2e
Holly <hollyabel@hotmail.co.uk>
2026-01-01 14:55:18 -0500

    Pushing changes to CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,20 +4,44 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    numOfItems: 0,
   },
+
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+            
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost, quantity: 1 });
+        }
+      
+        state.numOfItems += 1;      
     },
+
     removeItem: (state, action) => {
+        const name = action.payload;
+        const itemToRemove = state.items.find(item => item.name === name);
+            
+        if (itemToRemove) {
+            state.numOfItems -= itemToRemove.quantity;    
+            }	
+            state.items = state.items.filter(item => item.name !== name);            
     },
+
     updateQuantity: (state, action) => {
-
-    
-    },
-  },
-});
-
+        const { name, quantity } = action.payload;
+    	const existingItem = state.items.find(item => item.name === name);
+           
+        if (existingItem) {
+            const differenceQuantity = quantity - existingItem.quantity;
+            existingItem.quantity = quantity;
+            state.numOfItems += differenceQuantity;
+          }
+       },
+      });
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
 export default CartSlice.reducer;
